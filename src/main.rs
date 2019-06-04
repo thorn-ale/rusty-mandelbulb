25a740d0f2d2da18f40f32a9322daee25745f770
Léo Rousseau <rousseauleo1@gmail.com>
2019-06-04 17:09:03 +0200

    Implementation of parallelism with rayon ; rewrote main.rs in more idiomatic rust

--- src/main.rs
+++ src/main.rs
@@ -26,14 +26,29 @@ use num::{Float, FromPrimitive};
 extern crate png;
 use png::HasParameters;
 
+extern crate rayon;
+use rayon::prelude::*;
+
 use std::error::Error;
 use std::fs::File;
 use std::io::BufWriter;
 use std::path::Path;
 
-const STEP: u32 = 100;
+
 const EXPONENT: u32 = 8;
 const MAX_ITER: u32 = 10;
+
+const X_MIN: f64 = -1.5;
+const X_MAX: f64 = 1.0;
+const X_STEP: u32 = 1000;
+
+const Y_MIN: f64 = -1.5;
+const Y_MAX: f64 = 1.5;
+const Y_STEP: u32 = 1000;
+
+const Z_MIN: f64 = -1.5;
+const Z_MAX: f64 = 1.5;
+const Z_STEP: u32 = 1000;
 
 fn linspace<T>(start: T, stop: T, nstep: u32) -> Vec<T>
 where
@@ -41,11 +56,12 @@ where
 {
     let delta: T = (stop - start) / T::from_u32(nstep - 1).expect("out of range");
     return (0..(nstep))
-        .map(|i| start + T::from_u32(i).expect("out of range") * delta)
+        .map(|i| start + T::from_u32(i)
+        .expect("out of range") * delta)
         .collect();
 }
 
-fn mandelbulb(x: f64, y: f64, z: f64, exponent: u32, max_iter: u32) -> bool {
+fn mandelbulb(x: f64, y: f64, z: f64) -> bool {
 	let mut x0 = x;
 	let mut y0 = y;
 	let mut z0 = z;
@@ -54,7 +70,7 @@ fn mandelbulb(x: f64, y: f64, z: f64, exponent: u32, max_iter: u32) -> bool {
 	let mut theta: f64;
 	let mut phi: f64;
 
-	for _ in 0..max_iter {
+	for _ in 0..MAX_ITER {
 		r = (x0*x0 + y0*y0 + z0*z0).sqrt();
 		theta = (x0*x0 + y0*y0).sqrt().atan2(z0);
 		phi = y0.atan2(x0);
@@ -62,47 +78,47 @@ fn mandelbulb(x: f64, y: f64, z: f64, exponent: u32, max_iter: u32) -> bool {
 		if r > 24.0 {
 			return false;
 		}
-		let temp = r.powi(exponent as i32);
-		x0 += (theta * exponent as f64).sin() * (phi * exponent as f64).cos() * temp;
-		y0 += (theta * exponent as f64).sin() * (phi * exponent as f64).sin() * temp;
-		z0 += (theta * exponent as f64).cos() * temp;
+		let temp = r.powi(EXPONENT as i32);
+		x0 += (theta * EXPONENT as f64).sin() * (phi * EXPONENT as f64).cos() * temp;
+		y0 += (theta * EXPONENT as f64).sin() * (phi * EXPONENT as f64).sin() * temp;
+		z0 += (theta * EXPONENT as f64).cos() * temp;
 
 	}
 
 	true
 }
 
-fn main() {
-	let x_range = linspace(-1.5 as f64, 1.5 as f64, STEP);
-	let y_range = linspace(-1.5 as f64, 1.5 as f64, STEP);
-	let z_range = linspace(-1.5 as f64, 1.5 as f64, STEP);
-	let mut name = 1;
-	for x in x_range.iter(){
-		let str_name = "temp/".to_owned() + &name.to_string() + ".png";
-		let path = Path::new(&str_name);
-		let file = match File::create(&path) {
-			Err(why) => panic!("couldn't open: {}", why.description()),
-			Ok(file) => file
-		};
-		let ref mut w = BufWriter::new(file);
-		let mut encoder = png::Encoder::new(w, STEP, STEP);
-		encoder.set(png::ColorType::RGB).set(png::BitDepth::Eight);
-		let mut writer = encoder.write_header().unwrap();
-		let mut data = vec![];
-		for y in y_range.iter(){
-			for z in z_range.iter(){
-				if mandelbulb(*x, *y, *z, EXPONENT, MAX_ITER) {
-					data.push(255);
-					data.push(255);
-					data.push(255);
-				} else {
-					data.push(0);
-					data.push(0);
-					data.push(0);
-				}
+
+
+fn compute_slice(x: f64, id: usize){
+	let y_range = linspace(Y_MIN, Y_MAX, Y_STEP);
+	let z_range = linspace(Z_MIN, Z_MAX, Z_STEP);
+	let slice_name = "temp/".to_owned() + &id.to_string() + ".png";
+	let path = Path::new(&slice_name);
+	let file = match File::create(&path) {
+		Err(why) => panic!("couldn't open: {}", why.description()),
+		Ok(file) => file
+	};
+	let ref mut w = BufWriter::new(file);
+	let mut encoder = png::Encoder::new(w, Y_STEP, Z_STEP);
+	encoder.set(png::ColorType::RGB).set(png::BitDepth::Eight);
+	let mut writer = encoder.write_header().unwrap();
+	let mut data = vec![];
+	for y in y_range.iter(){
+		for z in z_range.iter(){
+			match mandelbulb(x, *y, *z) {
+				true => data.append(&mut vec![255, 255, 255]),
+				false => data.append(&mut vec![0, 0, 0])
 			}
 		}
-		writer.write_image_data(&data).unwrap();
-		name += 1;
 	}
+	writer.write_image_data(&data).unwrap();
+}
+
+fn main() {
+	let x_range = linspace(X_MIN, X_MAX, X_STEP);
+	x_range
+		.par_iter()
+		.enumerate()
+		.for_each(|(id, x)| compute_slice(*x, id));
 }
